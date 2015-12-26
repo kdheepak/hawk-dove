e1ce2bae01db2c52fae7fc39c40dd5edfd372800
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2015-12-26 00:40:27 -0700

    Update egt.py and species.py

--- egt.py
+++ egt.py
@@ -5,6 +5,9 @@ from __future__ import absolute_import, division, print_function
 from builtins import (bytes, str, open, super, range,
                       zip, round, input, int, pow, object)
 
+import matplotlib.pyplot as plt
+import pandas as pd
+
 import species
 
 
@@ -17,20 +20,71 @@ class Game(object):
     The individuals leave the game pairwise contest with a resulting fitness determined by the contest outcome – generally represented in a Payoff Matrix."""
     def __init__(self, *args, **kwargs):
         self.rules = Rules()
-        self.population = kwargs.pop('population', species.Population())
+        self.population = kwargs.pop('population')
+        self.total_resources = kwargs.pop('resources', 100)
+        self.exhaustion = kwargs.pop('exhaustion', 0)
+        self.limited_resources = kwargs.pop('limited_resources', False)
         self.period = 0
 
     def advance(self):
         self.period += 1
-        self.compete(self.population.random_individual, self.population.random_individual)
+        count = 0
+        self.current_resources = self.total_resources
+        while self.compete(self.population.random_individual, self.population.random_individual):
+            count = count + 1
+
+        while self.hurt(self.population.random_individual):
+            pass
+
         self.population.clean()
 
+    def hurt(self, species):
+        if species is None:
+            return False
+        else:
+            species.fitness -= self.exhaustion
+            return True
+
     def compete(self, species1, species2):
-        payoff = self.rules.payoff(species1, species2)
-        species1.fitness += payoff[0]
-        species2.fitness += payoff[1]
-        species1.fights += 1
-        species2.fights += 1
+        if self.current_resources < 0:
+            return False
+        elif species1 is None or species2 is None:
+            return False
+        else:
+            payoff = self.rules.payoff(species1, species2)
+            species1.fitness += payoff[0]
+            species2.fitness += payoff[1]
+            species1.fights += 1
+            species2.fights += 1
+            if self.limited_resources:
+                self.current_resources -= payoff[0]
+            return True
+
+    def visualize(self, title='Case', special=False):
+        if self.population.number_of_hawks == 0 or self.population.number_of_doves == 0:
+            special=True
+
+        if special:
+            fig, axs = plt.subplots(1, 1, figsize=(16, 5))
+            ax = axs
+        else:
+            fig, axs = plt.subplots(2, 1, figsize=(16, 10))
+            ax = axs[0]
+
+        df = pd.DataFrame(self.population.history, columns=['hawk', 'dove', '%hawk', '%dove'])
+
+        df['hawk'].plot(label='number of hawks', legend=True, ax=ax)
+        df['dove'].plot(label='number of doves', legend=True, ax=ax)
+        ax.set_title(title)
+        ax.set_ylim(0, ax.axis()[3]);
+
+        if not special:
+            ax = axs[1]
+            df['%hawk'].plot(label='percent of hawks', legend=True, ax=ax)
+            df['%dove'].plot(label='percent of doves', legend=True, ax=ax)
+            ax.set_ylim(0, 1);
+
+        return fig, axs
 
 class Rules(object):
     def __init__(self, *args, **kwargs):
@@ -44,20 +98,19 @@ class PayoffMatrix(object):
     def __init__(self, *args, **kwargs):
         self.species1_name = kwargs.pop('species1_name', 'Hawk')
         self.species2_name = kwargs.pop('species2_name', 'Dove')
-        self.V = kwargs.pop('V', 50)
+        self.V = kwargs.pop('V', 2)
         self.C = kwargs.pop('C', -10)
         self.B = kwargs.pop('B', 4)
         self.payoff = {
-                        (self.species1_name, self.species1_name): (self.V, self.C*10),
-                        (self.species1_name, self.species2_name): (self.V, 0),
-                        (self.species2_name, self.species1_name): (0, self.V),
-                        (self.species2_name, self.species2_name): (self.V+self.C, self.C),
+                        (self.species1_name, self.species1_name): (-2, -2),
+                        (self.species1_name, self.species2_name): (10, 0),
+                        (self.species2_name, self.species1_name): (0, 10),
+                        (self.species2_name, self.species2_name): (3, 3),
                     }
 
     def __getitem__(self, keys):
 
         species1_name = keys[0]
         species2_name = keys[1]
-
         return self.payoff[(species1_name, species2_name)]
 
--- species.py
+++ species.py
@@ -20,6 +20,7 @@ class Bird(object):
         self.fitness = kwargs.pop('fitness', 50)
         self.fights = kwargs.pop('fights', 0)
         self.species = kwargs.pop('species', self.__class__.__name__)
+        self.active = True
 
     def __str__(self):
         return("""{}\t{}\t{}""".format(self.species, self.fitness, self.fights))
@@ -43,34 +44,50 @@ class Population(object):
             number_of_doves (int)
         """
         self.individuals = [Hawk() for _ in range(0, kwargs.pop('number_of_hawks', 10))] + [Dove() for _ in range(0, kwargs.pop('number_of_doves', 10))]
-        self.THRESHOLD = kwargs.pop('THRESHOLD', 10000)
+        self.THRESHOLD = kwargs.pop('THRESHOLD', 100)
         self.total = len(self.individuals)
         self.history = []
+        self.clean()
 
     def describe(self):
-        total = self.number_of_hawks + self.number_of_doves
         print("Number of hawks = {}".format(self.number_of_hawks))
         print("Number of doves = {}".format(self.number_of_doves))
-        print("Percent of hawks = {}".format(self.number_of_hawks/total*100))
-        print("Percent of doves = {}".format(self.number_of_doves/total*100))
+        print("Percent of hawks = {}".format(self.number_of_hawks/self.total*100))
+        print("Percent of doves = {}".format(self.number_of_doves/self.total*100))
 
     @property
-    def random_individual(self):
-        return random.choice(self.individuals)
+    def random_individual(self, active=True):
+        try:
+            i = random.choice([individual for individual in self.individuals if individual.active==active])
+            i.active = False
+            return i
+        except IndexError:
+            return None
+
 
     def clean(self):
         self.individuals = [bird for bird in self.individuals if bird.fitness >= 0]
-
+        for bird in self.individuals:
+            bird.active = True
+        self.total = len(self.individuals)
         self.number_of_hawks = [bird.species for bird in self.individuals].count('Hawk')
         self.number_of_doves = [bird.species for bird in self.individuals].count('Dove')
 
-        self.history.append([self.number_of_hawks, self.number_of_doves])
+        if self.total == 0:
+            self.total = 1
+        self.history.append([self.number_of_hawks, self.number_of_doves, self.number_of_hawks/self.total, self.number_of_doves/self.total])
         self.breed()
 
     def breed(self):
-        breed_pool = []
+
         for individual in self.individuals:
-            if individual.fitness > self.THRESHOLD:
+            if individual.fitness >= 100:
+                individual.fitness = 100
+
+        breed_pool = []
+        # print("{} number of breeders exist".format(len([individual for individual in self.individuals if individual.fitness > self.THRESHOLD])))
+        for individual in self.individuals:
+            if individual.fitness >= self.THRESHOLD:
                 individual.fitness = individual.fitness / 2
                 breed_pool.append((individual.species, individual.fitness))
 
